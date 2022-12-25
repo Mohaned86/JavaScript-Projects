a164c0260f2c8707d6410dd9cced05e4e74e8bc7
Mohaned86 <90223271+Mohaned86@users.noreply.github.com>
2022-12-25 10:01:01 -0800

    This is first work in JavaScrpit

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+var X = ' This is the beginning of the string' + ' life is to short' + (' thank you.'), A = ' and this is the end of the string' ; // here I create tow vareables and concatente it 
+window.alert(A) ;  // here Iwant to pob ud yhis message
+document.write(X) ; //here I print tha value of X
+document.write(A);  //here I print the value of A
+document.write(X + A); // here I print the tow variable together
