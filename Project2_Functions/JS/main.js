cb4081703b3fbab57686d013d69724075f63b937
Mohaned86 <90223271+Mohaned86@users.noreply.github.com>
2022-12-25 17:08:00 -0800

    project2

---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,13 @@
+function My_function (a, b, c)  {
+    var a = str;
+    var b = str1;
+    var c = str + "" + str1;
+    str = "This is the alert Botton";
+    ste1 = "emergence";
+    document.getElementById("Botton").innerHTML = c;
+}
+function myfunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById(concatenate). innerHTML = sentence;
+}
