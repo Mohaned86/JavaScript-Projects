1ecde2ad67eb04378ded5a142e3f5527ab218f77
Mohaned86 <90223271+Mohaned86@users.noreply.github.com>
2022-12-27 19:19:52 -0800

    Main

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function dictionary() {
+    var Animal = {
+        Species:"Dog",
+        Color:"White",
+        Breed:"Labrador",
+        Age:5,
+        Sound:"Bark!",
+    }
+    document.getElementById("Dictionary").innerHTML=Animal.Sound;
+}
